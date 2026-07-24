5fd6a36e43b2fbef50bb35c081285b576f24b203
tranmanhlong556688-ctrl <tranmanhlong556688@gmail.com>
2026-07-24 20:43:23 +0700

    fix: target Android 13 API 33 on Redmi Lite

--- AutoTouchMinh/redmilite/build.gradle.kts
+++ AutoTouchMinh/redmilite/build.gradle.kts
@@ -21,7 +21,7 @@ android {
     defaultConfig {
         applicationId = "com.minh.autotouch.redmilite"
         minSdk = 26
-        targetSdk = 32
+        targetSdk = 33
         versionCode = 1
         versionName = "1.3.0-redmi-lite"
     }
